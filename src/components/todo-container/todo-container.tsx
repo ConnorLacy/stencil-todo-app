75cd1111be48cb064ad33abcaf8c7bb8a22ac8b2
ConnorLacy <lacyconnor1409@gmail.com>
2020-07-23 18:37:18 -0400

    Removed todos from state. Added listeners for more custom events

--- src/components/todo-container/todo-container.tsx
+++ src/components/todo-container/todo-container.tsx
@@ -1,18 +1,14 @@
 import {
   Component,
   ComponentInterface,
-  State,
   Element,
+  Host,
   h,
   Listen,
 } from "@stencil/core";
 
-import { TodoCheckedEvent } from "../todo-item/todo-item";
-
-type Todo = {
-  checked: boolean;
-  text: string;
-};
+import { TodoCompletedEvent, TodoReversed } from "../todo-item/todo-item";
+import { TodoCreatedEvent } from "../todo-creator/todo-creator";
 
 @Component({
   tag: "todo-container",
@@ -20,43 +16,48 @@ type Todo = {
   scoped: true,
 })
 export class TodoContainer implements ComponentInterface {
-  @State() todos: Todo[];
-  @Element() element;
-  @Listen("todoChecked")
-  handleTodoChecked(e: CustomEvent<TodoCheckedEvent>){
-    console.log(`Text from changed todo: ${e.detail.text}`)
+  // Still confused on this
+  @Element() el!: HTMLElement;
+
+  @Listen("todoCompleted")
+  handleTodoCompleted(e: CustomEvent<TodoCompletedEvent>) {
+    console.log(`Todo completed....nice: ${e.detail.text}`);
+    let todoList = document.querySelectorAll('[slot="todo-items')
   }
-  
-  componentWillLoad() {
-    this.todos = this.getTodos();
+
+  @Listen("todoCreated")
+  handleTodoCreated(e: CustomEvent<TodoCreatedEvent>) {
+    console.log(`I smell a new todo.....: ${e.detail.text}`);
+    let completedList = document.querySelector('[slot="todo-items"]');
+    let todoItem = document.createElement("todo-item");
+    todoItem.text = e.detail.text;
+    todoItem.checked = e.detail.checked;
+    completedList.appendChild(todoItem);
+  }
+
+  @Listen("todoReversed")
+  handleTodoReversed(e: CustomEvent<TodoReversed>) {
+    console.log(`You lazy mf...todo reversed: ${e.detail.text}`);
   }
 
   render() {
     return (
-      <div class="todos-container">
+      <Host>
         <h2>
           <slot name="title" />
         </h2>
-        <div>
-          <h3>Completed Todos</h3>
-          <slot name="completed-items">
-            <slot />
-          </slot>
-        </div>
-        <div>
+        <div role="listgroup">
           <h3>Incomplete Todos</h3>
-          <slot name="todo-items">
-            <slot />
-          </slot>
+          <slot name="todo-items"></slot>
         </div>
-      </div>
+        <div role="listgroup">
+          <h3>Completed Todos</h3>
+          <slot name="completed-items"></slot>
+        </div>
+        <div class="create-todo">
+          <slot name="create"></slot>
+        </div>
+      </Host>
     );
   }
-
-  getTodos = () =>
-    [].slice
-      .call(this.element.querySelectorAll(".todos-container > div > div"))
-      .map((container) => {
-        return [].slice.call(container.children);
-      });
 }
