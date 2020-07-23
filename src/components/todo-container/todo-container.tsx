a3e404d93d63129fd87483ee40124f44d1e72c93
ConnorLacy <lacyconnor1409@gmail.com>
2020-07-23 19:34:10 -0400

    added logic for moving todo items

--- src/components/todo-container/todo-container.tsx
+++ src/components/todo-container/todo-container.tsx
@@ -13,31 +13,38 @@ import { TodoCreatedEvent } from "../todo-creator/todo-creator";
 @Component({
   tag: "todo-container",
   styleUrl: "todo-container.css",
-  scoped: true,
 })
 export class TodoContainer implements ComponentInterface {
-  // Still confused on this
   @Element() el!: HTMLElement;
 
   @Listen("todoCompleted")
   handleTodoCompleted(e: CustomEvent<TodoCompletedEvent>) {
     console.log(`Todo completed....nice: ${e.detail.text}`);
-    let todoList = document.querySelectorAll('[slot="todo-items')
+    let element = e.target as HTMLElement;
+    let parent = element.parentElement;
+    parent.removeChild(element);
+    let completedList = document.querySelector('[slot="completed-items"]');
+    completedList.append(element);
   }
 
   @Listen("todoCreated")
   handleTodoCreated(e: CustomEvent<TodoCreatedEvent>) {
     console.log(`I smell a new todo.....: ${e.detail.text}`);
-    let completedList = document.querySelector('[slot="todo-items"]');
+    let todoList = this.getTodoList();
     let todoItem = document.createElement("todo-item");
     todoItem.text = e.detail.text;
     todoItem.checked = e.detail.checked;
-    completedList.appendChild(todoItem);
+    todoList.appendChild(todoItem);
   }
 
   @Listen("todoReversed")
   handleTodoReversed(e: CustomEvent<TodoReversed>) {
     console.log(`You lazy mf...todo reversed: ${e.detail.text}`);
+    let element = e.target as HTMLElement;
+    let parent = element.parentElement;
+    parent.removeChild(element);
+    let todoList = this.getTodoList();
+    todoList.append(element);
   }
 
   render() {
@@ -60,4 +67,8 @@ export class TodoContainer implements ComponentInterface {
       </Host>
     );
   }
+
+  getTodoList = () => {
+    return document.querySelector('[slot="todo-items"]');
+  };
 }
